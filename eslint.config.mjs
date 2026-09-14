01234d203c3da7b38ecec178baf40d5f0cc07a88
Benjamin Rush <bnjmnrsh@gmail.com>
2026-09-14 19:20:57 +0100

    chore: preserve local ESLint exception

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,10 @@
 import ranWordPress from '@rocketsarenostalgic/quality-config/eslint/wordpress';
 
-export default [...ranWordPress];
+export default [
+	...ranWordPress,
+	{
+		rules: {
+			'@wordpress/no-unsafe-wp-apis': 'off',
+		},
+	},
+];
